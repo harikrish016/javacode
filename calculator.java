f2f3777d3f4d29f28b1bc77b1c4ad8e877bfd9c9
harikrish016 <104581414+harikrish016@users.noreply.github.com>
2022-10-05 15:03:59 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,15 @@
+class calculator1
+{
+void string()
+{
+System.out.println("helloworld");
+}
+}
+class calculator
+{
+public static void main(String args[])
+{
+calculator1 calci=new calculator1();
+System.out.println(calci.add());
+}
+}
