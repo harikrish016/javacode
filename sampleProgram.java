1a7d67b029e556ac808de498980713ad61aa15d7
harikrish016 <104581414+harikrish016@users.noreply.github.com>
2022-10-08 15:55:14 +0530

    Add files via upload

---
+++ sampleProgram.java
@@ -0,0 +1,21 @@
+class mathsCalculation
+{
+int x;
+int y;
+double a;
+double b;
+
+void add()
+{
+b=x+y;
+System.out.println(b);
+}
+}
+class sampleProgram
+{
+public static void main(String args[])
+{
+mathsCalculation hari=new mathsCalculation();
+hari.x=5;hari.y=10;
+hari.add();
+}}
